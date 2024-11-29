2723120485214766364165d953b8900a4a10c1ca
Ayush Surana <suranaayush97@gmail.com>
2024-11-30 03:18:21 +0530

    Update ILoginService.java

--- user-management/src/main/java/com/pheonix/user/management/service/ILoginService.java
+++ user-management/src/main/java/com/pheonix/user/management/service/ILoginService.java
@@ -11,6 +11,7 @@ public interface ILoginService {
 
     UserAuthSession signUp(UserRequest userRequest)throws PheonixException;
     UserAuthSession loginSignUp(UserRequest userRequest)throws PheonixException;
+    UserAuthSession loginSignUp(UserRequest userRequest)throws PheonixException;
     UserSessionVO auth(UserRequest userRequest)throws PheonixException;
     UserSessionVO getSessionData(String sessionId) throws PheonixException;
 }
