44441bcd0fe008e0d610d45c79405c1c93418e1a
Ayush Surana <suranaayush97@gmail.com>
2024-11-30 03:23:25 +0530

    Dupliocate Error

--- user-management/src/main/java/com/pheonix/user/management/service/ILoginService.java
+++ user-management/src/main/java/com/pheonix/user/management/service/ILoginService.java
@@ -12,6 +12,7 @@ public interface ILoginService {
     UserAuthSession signUp(UserRequest userRequest)throws PheonixException;
     UserAuthSession loginSignUp(UserRequest userRequest)throws PheonixException;
     UserAuthSession loginSignUp(UserRequest userRequest)throws PheonixException;
+    UserAuthSession loginSignUp(UserRequest userRequest)throws PheonixException;
     UserSessionVO auth(UserRequest userRequest)throws PheonixException;
     UserSessionVO getSessionData(String sessionId) throws PheonixException;
 }
